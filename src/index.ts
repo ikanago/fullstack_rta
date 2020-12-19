d900016731b6ea159907aa586b3f8413cf8b3623
ikanago <28985004+ikanago@users.noreply.github.com>
2020-12-19 22:30:47 +0900

    feat: Define database schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    const post = orm.em.create(Post, {title: 'My first post'});
+    await orm.em.persistAndFlush(post);
+}
+
+main().catch(err => {
+    console.error(err);
+});
