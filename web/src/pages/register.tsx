cb90e5f5237ff235ab48e2baa330ff5f258772e8
ikanago <28985004+ikanago@users.noreply.github.com>
2020-12-20 17:21:07 +0900

    feat: Register page

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+import { Box, Button } from "@chakra-ui/react";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ name: "Sasuke" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">
+                            register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
+};
+
+export default Register;
