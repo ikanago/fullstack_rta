772fa2d65d3125c9481a4d7a38caccbb82d90c86
ikanago <28985004+ikanago@users.noreply.github.com>
2020-12-20 11:35:34 +0900

    fix: Add command for CI

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const post = orm.em.create(Post, { title: "Hoge"});
+    const post = orm.em.create(Post, { title: "Hoge" });
     await orm.em.persistAndFlush(post);
     const posts = await orm.em.find(Post, {});
     console.log(posts);
